92662bcc00f365b8056dbc86afbc993e1f39f3e6
pfriteau <pfriteau@hotmail.fr>
2023-10-03 21:33:54 +0200

    Initial Commit for McDiscGolf Fabric Mod

---
+++ src/main/java/net/jaipaul/mcdiscgolf/McDiscGolfClient.java
@@ -0,0 +1,10 @@
+package net.jaipaul.mcdiscgolf;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class McDiscGolfClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
